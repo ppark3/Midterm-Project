d61bfcc8ffc44fa84ff78be4e354ffa39fac7b18
Philip Park <philippark0203@gmail.com>
2019-10-01 17:01:34 -0400

    Second Commit just in case!

---
+++ Assets/MusicManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicManager : MonoBehaviour
+{
+    public bool goodDancing;
+    public bool goodPlaying;
+    public bool badDancing;
+    public bool badPlaying;
+
+    public AudioSource audioSource;
+    public AudioClip goodSong;
+    public AudioClip badSong;
+
+    public bool recordedGoodTime;
+    public float goodSongTime;
+    public bool recordedBadTime;
+    public float badSongTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        goodSongTime = 0f;
+        goodPlaying = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (goodDancing && !goodPlaying)
+        {
+            audioSource.clip = goodSong;
+            audioSource.time = goodSongTime;
+            audioSource.Play();
+            goodPlaying = true;
+        }
+        if (goodDancing)
+        {
+            goodSongTime = audioSource.time;
+        }
+        if (!goodDancing)
+        {
+            audioSource.Stop();
+            goodPlaying = false;
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerBehavior.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBehavior : MonoBehaviour
+{
+    public float moveSpeed;
+    public bool dancing;
+
+    public GameObject musicManager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W)) // moving up
+        {
+            this.transform.Translate(new Vector3(0, 0, -moveSpeed * Time.deltaTime));
+        }
+        if (Input.GetKey(KeyCode.A)) // moving left
+        {
+            this.transform.Translate(new Vector3(moveSpeed * Time.deltaTime, 0, 0));
+        }
+        if (Input.GetKey(KeyCode.D)) // moving right
+        {
+            this.transform.Translate(new Vector3(-moveSpeed * Time.deltaTime, 0, 0));
+        }
+        if (Input.GetKey(KeyCode.S)) // moving down
+        {
+            this.transform.Translate(new Vector3(0, 0, moveSpeed * Time.deltaTime));
+        }
+        if (Input.GetKeyDown(KeyCode.J))
+        {
+            musicManager.gameObject.GetComponent<MusicManager>().goodDancing = true;
+        }
+        if (Input.GetKeyUp(KeyCode.J))
+        {
+            musicManager.gameObject.GetComponent<MusicManager>().goodDancing = false;
+        }
+    }
+}
