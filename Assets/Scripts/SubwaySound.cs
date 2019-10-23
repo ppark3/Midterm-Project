4b7d6021dbbfaa97e418cf5822f2fc39ca47b660
Philip Park <philippark0203@gmail.com>
2019-10-23 00:55:29 -0400

    Added in all models from Blender!

---
+++ Assets/Scripts/SubwaySound.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SubwaySound : MonoBehaviour
+{
+    public static SubwaySound _SubwaySounds;
+
+    private void Awake()
+    {
+        if (_SubwaySounds == null)
+        {
+            _SubwaySounds = this;
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
